6016e802f5a9ca2d067c3315e7d1980550bf2444
bjausseran <benoit.jausseran@ynov.com>
2021-10-17 19:18:37 +0200

    Unity project added

---
+++ Assets/Scripts/MapGenerator/Data/TreeData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+[CreateAssetMenu()]
+public class TreeData : UpdatableData {
+
+	public Layer[] layers;
+
+	float savedMinHeight;
+	float savedMaxHeight;
+
+
+
+	[System.Serializable]
+	public class Layer {
+		public GameObject visual;
+		[Range(0,1)]
+		public float startHeight;
+		[Range(0, 1)]
+		public float endHeight;
+		[Range(0,1)]
+		public float blendStrength;
+	}
+		
+	 
+}
